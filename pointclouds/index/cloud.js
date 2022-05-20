f1533dff86a1002599dd3b3faa557e8350f52144
unknown <gallardoirg@gmail.com>
2022-05-20 15:24:52 -0500

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 96292898,
+    "boundingBox": {
+        "lx": 480117.01200927738,
+        "ly": 2208094.7167199708,
+        "lz": 2173.889452667236,
+        "ux": 480816.94731201176,
+        "uy": 2208794.652022705,
+        "uz": 2873.824755401611
+    },
+    "tightBoundingBox": {
+        "lx": 480117.012,
+        "ly": 2208094.7167,
+        "lz": 2173.8894999999998,
+        "ux": 480805.3112,
+        "uy": 2208794.652,
+        "uz": 2294.0833
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 6.061617374420166,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
